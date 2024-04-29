008b56fb4a3440fa75981fed3c9fb54c58535406
OcheOps <ochecodes@gmail.com>
2024-04-29 15:11:09 +0100

    Finally finished

--- models/projectProgress.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type ProjectProgress struct {
-    ProjectName     string   `bson:"projectName"`
-    TasksAccomplished []string `bson:"tasksAccomplished"`
-    PendingTasks    []string `bson:"pendingTasks"`
-    Constraints     []string `bson:"constraints"`
-    Remarks         string   `bson:"remarks"`
-}
---
+++ models/project_details.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ProjectDetails struct {
+    ID                 primitive.ObjectID `bson:"_id,omitempty"`
+    PTBReference       string             `bson:"ptbReference"`
+    ProjectName        string             `bson:"projectName"`
+    AwardExecutionPeriod string           `bson:"awardExecutionPeriod"`
+    ProjectSupervisor  string             `bson:"projectSupervisor"`
+}
